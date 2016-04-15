7eae015587014027274b319c13a0d76e33eb2864
Andrew <andrew.mitchell@zentri.com>
2016-04-15 12:02:27 +1000

    proto -> class

--- index.js
+++ index.js
@@ -1,60 +1,63 @@
 "use strict";
 
-var ws = require('ws');
-var logger = require('winston');
-var watch = require('glob-watcher');
-var _ = require('lodash');
+const ws = require('ws');
+const logger = require('winston');
+const watch = require('glob-watcher');
+const _ = require('lodash');
 
-function tumbleweed(opts) {
-  logger.info('\n  # #\n######\n###### tumbleweed on port ' + opts.port + '\n # #');
+class tumbleweed {
+  constructor(opts) {
+    logger.info('\n  # #\n######\n###### tumbleweed on port ' + opts.port + '\n # #');
 
-  var self = this;
-  this.opts = opts;
+    var self = this;
+    this.opts = opts;
 
-  this.wss = new ws.Server(opts);
+    this.wss = new ws.Server(opts);
 
-  this.wss.on('connection', function connection(ws) {
-    var clientID = ws._socket._handle.fd;
+    this.wss.on('connection', function connection(ws) {
+      var clientID = ws._socket._handle.fd;
 
-    ws.on('message', function incoming(msg) {
-      logger.info('spore %s: %s', clientID, msg);
+      ws.on('message', function incoming(msg) {
+        logger.info('spore %s: %s', clientID, msg);
 
-      try {
-        msg = JSON.parse(msg);
-      } catch(e) {
-        logger.warn('invalid json from spore');
-        return;
-      }
+        try {
+          msg = JSON.parse(msg);
+        } catch(e) {
+          logger.warn('invalid json from spore');
+          return;
+        }
 
-      if(msg.location || msg.command){
-        //rebroadcast to other clients
-        logger.info('broadcast', msg);
-        self.broadcast(msg, clientID);
-      }
+        if(msg.location || msg.command){
+          //rebroadcast to other clients
+          logger.info('broadcast', msg);
+          self.broadcast(msg, clientID);
+        }
+      });
+
+      ws.on('close', function(){});
     });
 
-    ws.on('close', function(){});
-  });
+    // this.watcher = watch(opts.watch);
+    this.watcher = watch(opts.watch);
+    this.watcher.on('change', function(){
+      self.broadcast(JSON.stringify({update:true}));
+    });
 
-  // this.watcher = watch(opts.watch);
-  this.watcher = watch(opts.watch);
-  this.watcher.on('change', function(){
-    self.broadcast(JSON.stringify({update:true}));
-  });
-
-  this.quit = this.wss.close;
-}
-
-tumbleweed.prototype.broadcast = function (msg, sender) {
-  var clients = this.wss.clients;
-  if(sender) {
-    clients = _.filter(clients, function(client){return client._socket._handle.fd !== sender});
+    this.quit = this.wss.close;
   }
 
-  clients.forEach(function(client) {
-    client.send(JSON.stringify(msg));
-  });
-};
+  broadcast(msg, sender) {
+    var clients = this.wss.clients;
+    if(sender) {
+      clients = _.filter(clients, function(client){return client._socket._handle.fd !== sender});
+    }
+
+    clients.forEach(function(client) {
+      client.send(JSON.stringify(msg));
+    });
+  }
+}
+
 
 module.exports = function(opts){
   return new tumbleweed(opts);
